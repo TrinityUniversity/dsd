872f32a0e7351c30d64dd57589082c1e3e540132
Ralph Minderhoud <ralphminderhoud@gmail.com>
2014-03-18 21:48:19 -0500

    Crude implementation of old version of drawing JS to provide foundation for next update

--- dsd/dsd.py
+++ dsd/dsd.py
@@ -9,52 +9,28 @@ from xblock.fragment import Fragment
 
 class DsdXBlock(XBlock):
     """
-    TO-DO: document what your XBlock does.
+        TO-DO: document what your XBlock does.
     """
 
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
+    def studio_view(self, context=None):
+        return student_view(self, context)
 
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
-    )
-
-    def resource_string(self, path):
-        """Handy helper for getting resources from our kit."""
-        data = pkg_resources.resource_string(__name__, path)
-        return data.decode("utf8")
-
-    # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
-        """
-        The primary view of the DsdXBlock, shown to students
-        when viewing courses.
-        """
         html = self.resource_string("static/html/dsd.html")
         frag = Fragment(html.format(self=self))
         frag.add_css(self.resource_string("static/css/dsd.css"))
+        frag.add_javascript(self.resource_string("static/libs/kinetic/5.0.1/kinetic.min.js"))
         frag.add_javascript(self.resource_string("static/js/src/dsd.js"))
         frag.initialize_js('DsdXBlock')
-        # return frag
-        return Fragment(u"Hello, world!")
+        return frag
 
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
-    @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
+    def resource_string(self, path):
         """
-        An example handler, which increments the data.
+            Handy helper for getting resources from our kit.
         """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
+        data = pkg_resources.resource_string(__name__, path)
+        return data.decode("utf8")
 
-        self.count += 1
-        return {"count": self.count}
-
-    # TO-DO: change this to create the scenarios you'd like to see in the
-    # workbench while developing your XBlock.
     @staticmethod
     def workbench_scenarios():
         """A canned scenario for display in the workbench."""
@@ -62,8 +38,48 @@ class DsdXBlock(XBlock):
             ("DsdXBlock",
              """<vertical_demo>
                 <dsd/>
-                <dsd/>
-                <dsd/>
                 </vertical_demo>
              """),
         ]
+
+    # Fields are defined on the class.  You can access them in your code as
+    # self.<fieldname>.
+
+    # TO-DO: delete count, and define your own fields.
+    # count = Integer(
+    #     default=0, scope=Scope.user_state,
+    #     help="A simple counter, to show something happening",
+    # )
+
+    # def resource_string(self, path):
+    #     """Handy helper for getting resources from our kit."""
+    #     data = pkg_resources.resource_string(__name__, path)
+    #     return data.decode("utf8")
+
+    # # TO-DO: change this view to display your data your own way.
+    # def student_view(self, context=None):
+    #     """
+    #     The primary view of the DsdXBlock, shown to students
+    #     when viewing courses.
+    #     """
+    #     html = self.resource_string("static/html/dsd.html")
+    #     frag = Fragment(html.format(self=self))
+    #     frag.add_css(self.resource_string("static/css/dsd.css"))
+    #     frag.add_javascript(self.resource_string("static/js/src/dsd.js"))
+    #     frag.initialize_js('DsdXBlock')
+    #     # return frag
+    #     return Fragment(u"Hello, world!")
+
+    # # TO-DO: change this handler to perform your own actions.  You may need more
+    # # than one handler, or you may not need any handlers at all.
+    # @XBlock.json_handler
+    # def increment_count(self, data, suffix=''):
+    #     """
+    #     An example handler, which increments the data.
+    #     """
+    #     # Just to show data coming in...
+    #     assert data['hello'] == 'world'
+
+    #     self.count += 1
+    #     return {"count": self.count}
+
--- dsd/static/js/src/dsd.js
+++ dsd/static/js/src/dsd.js
@@ -17,6 +17,600 @@ function DsdXBlock(runtime, element) {
     });
 
     $(function ($) {
-        /* Here's where you'd do things on page load. */
+            
+      function writeMessage(message) {
+        //text.setText(message);
+        staticLayer.draw();
+      }
+      
+      var Ralph = 0;
+      var Daniel = 28;
+      var George = 22; 
+      var Jin = 21;
+      
+      var text = new Kinetic.Text({
+        x: 10,
+        y: 10,
+        //text: 'Static Layer',
+        fontSize: '30',
+        fontFamily: 'Calibri',
+        fill: 'black'
+      });
+      
+      var stage = new Kinetic.Stage({
+        container: 'dsd_block',
+        width: 800,
+        height: 500
+      });
+      var animLayer = new Kinetic.Layer();
+      var staticLayer = new Kinetic.Layer();
+
+      var selectbackground = new Kinetic.Rect({
+        x: 0,
+        y: 0,
+        width: 200, 
+        height: stage.height(),
+        fill: '#555555',
+        stroke: 'black',
+        strokeWidth: 4
+      });
+      
+      selectbackground.on('mousemove', function() {
+        var mousePos = stage.getPointerPosition();
+        var x = mousePos.x;
+        var y = mousePos.y;
+        writeMessage('x: ' + x + ', y: ' + y + ', Ralph: '+ Ralph);
+      });
+      
+      selectbackground.on('click', function() {
+        Ralph = 0;
+      });
+      
+        var background = new Kinetic.Rect({
+        x: 200,
+        y: 0,
+        width: 600,
+        height: stage.height(),
+        fill: '#888888',
+        stroke: 'black',
+        strokeWidth: 4
+        });
+        
+        background.on('mousemove', function() {
+        var mousePos = stage.getPointerPosition();
+        var x = mousePos.x;
+        var y = mousePos.y;
+        writeMessage('x: ' + x + ', y: ' + y);
+      });
+        
+        background.on('click', function() {
+            var mousePos = stage.getPointerPosition();
+            var x = mousePos.x;
+            var y = mousePos.y;
+            
+            if(Ralph == 1){
+            
+            var newgroup = new Kinetic.Group({
+                draggable: true
+            });
+            var singleNodeA = new Kinetic.Rect({
+                x: mousePos.x - 25,
+                y: mousePos.y - 25,
+                width: 75,
+                height: 50,
+                fill: '#00D2FF',
+                stroke: 'black',
+                strokeWidth: 4
+                //draggable: true
+            });
+            
+            var singleNodeB = new Kinetic.Rect({
+                x: mousePos.x + 50,
+                y: mousePos.y - 25,
+                width: 25,
+                height: 50,
+                fill: '#00D2FF',
+                stroke: 'black',
+                strokeWidth: 4
+                //draggable: true
+            });
+            
+            var dataText = new Kinetic.Text({
+                x: mousePos.x + 15,
+                y: mousePos.y - 10,
+                text: Daniel,
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+            });
+            
+            dataText.on('click', function() {
+            
+            var number = prompt("Node data: ", "");
+            this.setText(number);
+            animLayer.draw();
+            });
+            
+            var connecText = new Kinetic.Text({
+                x: mousePos.x + 57.5,
+                y: mousePos.y - 10,
+                text: '/',
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+            });
+            
+            connecText.on('click', function() {
+            
+            var number = prompt("Node data: ", "");
+            this.setText(number);
+            animLayer.draw();
+            });
+            
+            newgroup.add(singleNodeA);
+            newgroup.add(singleNodeB);
+            newgroup.add(dataText);
+            newgroup.add(connecText);
+            animLayer.add(newgroup);
+            }
+            
+            if(Ralph == 2){
+            
+            var newgroup = new Kinetic.Group({
+                draggable: true
+            });
+            var doubleNodeA = new Kinetic.Rect({
+                x: mousePos.x,
+                y: mousePos.y - 25,
+                width: 50,
+                height: 50,
+                fill: 'red',
+                stroke: 'black',
+                strokeWidth: 4
+                //draggable: true
+            });
+            
+            var doubleNodeB = new Kinetic.Rect({
+                x: mousePos.x + 50,
+                y: mousePos.y - 25,
+                width: 25,
+                height: 50,
+                fill: 'red',
+                stroke: 'black',
+                strokeWidth: 4
+                //draggable: true
+            });
+            
+            var doubleNodeC = new Kinetic.Rect({
+                x: mousePos.x - 25,
+                y: mousePos.y - 25,
+                width: 25,
+                height: 50,
+                fill: 'red',
+                stroke: 'black',
+                strokeWidth: 4
+                //draggable: true
+            });
+            
+            var dataText = new Kinetic.Text({
+                x: mousePos.x + 15,
+                y: mousePos.y - 10,
+                text: Jin,
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+            });
+            
+            dataText.on('click', function() {
+            
+            var number = prompt("Node data: ", "");
+            this.setText(number);
+            animLayer.draw();
+            });
+            
+            var connecText1 = new Kinetic.Text({
+                x: mousePos.x + 57.5,
+                y: mousePos.y - 10,
+                text: '/',
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+            });
+            
+            connecText1.on('click', function() {
+            
+            var number = prompt("Node data: ", "");
+            this.setText(number);
+            animLayer.draw();
+            });
+
+            var connecText2 = new Kinetic.Text({
+                x: mousePos.x - 17.5,
+                y: mousePos.y - 10,
+                text: '/',
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+            });
+            
+            connecText2.on('click', function() {
+            
+            var number = prompt("Node data: ", "");
+            this.setText(number);
+            animLayer.draw();
+            });
+            
+            newgroup.add(doubleNodeA);
+            newgroup.add(doubleNodeB);
+            newgroup.add(doubleNodeC);
+            newgroup.add(dataText);
+            newgroup.add(connecText1);
+            newgroup.add(connecText2);
+            animLayer.add(newgroup);
+            }
+            
+            if(Ralph == 3){
+            var newgroup = new Kinetic.Group({
+                draggable: true
+            });
+            var topRec = new Kinetic.Rect({
+            
+                x: mousePos.x - 37.5,
+                y: mousePos.y - 25,
+                width: 75,
+                height: 25,
+                fill: 'green',
+                stroke: 'black',
+                strokeWidth: 4
+            
+            });
+            
+            var botLeftRec = new Kinetic.Rect({
+            
+                x: mousePos.x - 37.5,
+                y: mousePos.y,
+                width: 37.5,
+                height: 25,
+                fill: 'green',
+                stroke: 'black',
+                strokeWidth: 4
+            
+            });
+            
+            var botRightRec = new Kinetic.Rect({
+            
+                x: mousePos.x,
+                y: mousePos.y,
+                width: 37.5,
+                height: 25,
+                fill: 'green',
+                stroke: 'black',
+                strokeWidth: 4
+            
+            });
+            
+            var dataText = new Kinetic.Text({
+                x: mousePos.x - 10,
+                y: mousePos.y - 22.5,
+                text: George,
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+                //draggable: true
+            });
+            
+            dataText.on('click', function() {
+            
+            var number = prompt("Node data: ", "");
+            this.setText(number);
+            animLayer.draw();
+            });
+
+            var lefText = new Kinetic.Text({
+                x: mousePos.x - 22.5,
+                y: mousePos.y + 2,
+                text: '/',
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+                //draggable: true
+            });
+            
+            lefText.on('click', function() {
+            
+            var number = prompt("Node data: ", "");
+            this.setText(number);
+            animLayer.draw();
+            });
+            
+            var righText = new Kinetic.Text({
+                x: mousePos.x + 15,
+                y: mousePos.y + 2,
+                text: '/',
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+                //draggable: true
+            });
+            
+            righText.on('click', function() {
+            
+            var number = prompt("Node data: ", "");
+            this.setText(number);
+            animLayer.draw();
+            });
+            
+            newgroup.add(topRec);
+            newgroup.add(botLeftRec);
+            newgroup.add(botRightRec);
+            newgroup.add(dataText);
+            newgroup.add(lefText);
+            newgroup.add(righText);
+            animLayer.add(newgroup);
+            }
+      });
+            var bluegroup = new Kinetic.Group({
+            });
+            var singleNodeA = new Kinetic.Rect({
+                x: 100 - 50,
+                y: 100 - 25,
+                width: 75,
+                height: 50,
+                fill: '#00D2FF',
+                stroke: 'black',
+                strokeWidth: 4
+                //draggable: true
+            });
+            
+            var singleNodeB = new Kinetic.Rect({
+                x: 100 + 25,
+                y: 100 - 25,
+                width: 25,
+                height: 50,
+                fill: '#00D2FF',
+                stroke: 'black',
+                strokeWidth: 4
+                //draggable: true
+            });
+            
+            var dataText = new Kinetic.Text({
+                x: 100 - 10,
+                y: 100 - 10,
+                text: Daniel,
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+            });
+            
+            var connecText = new Kinetic.Text({
+                x: 100 + 32.5,
+                y: 100 - 10,
+                text: '/',
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+            });
+            
+        bluegroup.on('click', function() {
+            Ralph = 1;
+      });
+            
+            bluegroup.add(singleNodeA);
+            bluegroup.add(singleNodeB);
+            bluegroup.add(dataText);
+            bluegroup.add(connecText);
+            
+            
+            var redgroup = new Kinetic.Group({
+            });
+            var doubleNodeA = new Kinetic.Rect({
+                x: 75,
+                y: 200 - 25,
+                width: 50,
+                height: 50,
+                fill: 'red',
+                stroke: 'black',
+                strokeWidth: 4
+                //draggable: true
+            });
+            
+            var doubleNodeB = new Kinetic.Rect({
+                x: 75 + 50,
+                y: 200 - 25,
+                width: 25,
+                height: 50,
+                fill: 'red',
+                stroke: 'black',
+                strokeWidth: 4
+                //draggable: true
+            });
+            
+            var doubleNodeC = new Kinetic.Rect({
+                x: 75 - 25,
+                y: 200 - 25,
+                width: 25,
+                height: 50,
+                fill: 'red',
+                stroke: 'black',
+                strokeWidth: 4
+                //draggable: true
+            });
+            
+            var dataText = new Kinetic.Text({
+                x: 75 + 15,
+                y: 200 - 10,
+                text: Jin,
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+            });
+            
+            var connecText1 = new Kinetic.Text({
+                x: 75 + 57.5,
+                y: 200 - 10,
+                text: '/',
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+            });
+
+            var connecText2 = new Kinetic.Text({
+                x: 75 - 17.5,
+                y: 200 - 10,
+                text: '/',
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+            });
+            
+            redgroup.add(doubleNodeA);
+            redgroup.add(doubleNodeB);
+            redgroup.add(doubleNodeC);
+            redgroup.add(dataText);
+            redgroup.add(connecText1);
+            redgroup.add(connecText2);
+      
+        redgroup.on('click', function() {
+            Ralph = 2;
+      });
+      
+            var greengroup = new Kinetic.Group({
+            });
+            var topRec = new Kinetic.Rect({
+            
+                x: 100 - 37.5,
+                y: 300 - 25,
+                width: 75,
+                height: 25,
+                fill: 'green',
+                stroke: 'black',
+                strokeWidth: 4
+            
+            });
+            
+            var botLeftRec = new Kinetic.Rect({
+            
+                x: 100 - 37.5,
+                y: 300,
+                width: 37.5,
+                height: 25,
+                fill: 'green',
+                stroke: 'black',
+                strokeWidth: 4
+            
+            });
+            
+            var botRightRec = new Kinetic.Rect({
+            
+                x: 100,
+                y: 300,
+                width: 37.5,
+                height: 25,
+                fill: 'green',
+                stroke: 'black',
+                strokeWidth: 4
+            
+            });
+            
+            var dataText = new Kinetic.Text({
+                x: 100 - 10,
+                y: 300 - 22.5,
+                text: George,
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+                //draggable: true
+            });
+
+            var lefText = new Kinetic.Text({
+                x: 100 - 22.5,
+                y: 300 + 2,
+                text: '/',
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+                //draggable: true
+            });
+            
+            var righText = new Kinetic.Text({
+                x: 100 + 15,
+                y: 300 + 2,
+                text: '/',
+                fontSize: '20',
+                fontFamily: 'Calibri',
+                fill: 'black'
+                //draggable: true
+            });
+            
+            greengroup.add(topRec);
+            greengroup.add(botLeftRec);
+            greengroup.add(botRightRec);
+            greengroup.add(dataText);
+            greengroup.add(lefText);
+            greengroup.add(righText);
+      
+        greengroup.on('click', function() {
+            Ralph = 3;
+      });
+      
+      animLayer.add(background);
+      staticLayer.add(text);
+      animLayer.add(selectbackground);
+      animLayer.add(bluegroup);
+      animLayer.add(redgroup);
+      animLayer.add(greengroup);
+      stage.add(animLayer).add(staticLayer);
+      stage.getContainer().addEventListener('mousedown', function(evt) {});
+
+      var period = 2000;
+      
+      var blueHex = new Kinetic.RegularPolygon({
+        x: stage.width()/2,
+        y: stage.height()/2,
+        sides: 6,
+        radius: 70,
+        fill: '#00D2FF',
+        stroke: 'black',
+        strokeWidth: 4,
+        draggable: true
+      });
+
+      var yellowHex = new Kinetic.RegularPolygon({
+        x: stage.width()/2,
+        y: stage.height()/2,
+        sides: 6,
+        radius: 70,
+        fill: 'yellow',
+        stroke: 'black',
+        strokeWidth: 4,
+        draggable: true
+      });
+
+      var redHex = new Kinetic.RegularPolygon({
+        x: 470,
+        y: stage.height()/2,
+        sides: 6,
+        radius: 70,
+        fill: 'red',
+        stroke: 'black',
+        strokeWidth: 4,
+        offset: {
+          x: 70,
+          y: 0
+        },
+        draggable: true
+      });
+      
+      var anim = new Kinetic.Animation(function(frame) {
+        var scale = Math.sin(frame.time * 2 * Math.PI / period) + 0.001;
+        // scale x and y
+        blueHex.scale({x:scale, y:scale});
+        // scale only x
+        yellowHex.scale({x:1, y:scale});
+        // scale only y
+        redHex.scale({x:scale, y: 1});
+      }, animLayer);
+
+      anim.start(); 
     });
 }
